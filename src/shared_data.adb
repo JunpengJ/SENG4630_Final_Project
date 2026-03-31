2e665bee1bf6f6c761ca47f7e68719764ed71bd0
Junpeng Jiang <jiangjunp@outlook.com>
2026-03-30 20:18:46 -0700

    Add target speed and throttle procedures to Shared_Data package

--- src/shared_data.adb
+++ src/shared_data.adb
@@ -36,6 +36,30 @@ package body Shared_Data is
          return Fault;
       end;
 
+      procedure Set_Target_Speed(Value : Float) is
+      begin
+         if Value >= 0.0  and Value <= 120 then
+            Target_Speed := Value;
+         end if;
+      end;
+
+      function Get_Target_Speed return Float is
+      begin
+         return Target_Speed;
+      end;
+
+      procedure Set_Throttle(Value : Float) is
+      begin
+         if Value >= -1.0  and Value <= 1.0 then
+            Throttle_Cmd := Value;
+         end if;
+      end;
+
+      function Get_Throttle return Float is
+      begin
+         return Throttle_Cmd;
+      end;
+
    end Shared;
 
 end Shared_Data;
--- src/shared_data.ads
+++ src/shared_data.ads
@@ -7,12 +7,36 @@ package Shared_Data is
       procedure Set_Distance(Value : Float);
       function Get_Distance return Float;
 
+      procedure Set_Target_Speed(Value : Float);
+      function Get_Target_Speed return Float;
+
+      procedure Set_Throttle(Value : Float);
+      function Get_Throttle return Float;
+
       procedure Set_Fault(Status : Boolean);
       function Get_Fault return Boolean;
+
+      procedure Set_Autopilot_Enabled(Enabled : Boolean);
+      function Get_Autopilot_Enabled return Boolean;
+
+      procedure Set_Driver_Brak(Value  : Float);
+      function Get_Driver_Brak return Float;
+
+      procedure Set_Driver_Streering(Value  : Float);
+      function Get_Driver_Steering return Float;
+
+      function Is_Override_Active return Boolean;
+
    private
       Speed    : Float := 0.0;
       Distance : Float := 100.0;
+      Target_Speed : Float := 80.0;
+      Throttle_Cmd : Float := 0.0;
       Fault    : Boolean := False;
+
+      Autopilot_Enabled : Boolean := True;
+      Driver_Brak : Float := 0.0;
+      Driver_Steering : Float := 0.0;
    end Shared;
 
 end Shared_Data;
