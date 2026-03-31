f4a7539aaccf2871a4923369f0032a8f7b37471b
Junpeng Jiang <jiangjunp@outlook.com>
2026-03-30 20:33:11 -0700

    Enhance actuator and control tasks with improved fault handling and throttle management

--- src/actuator_task.adb
+++ src/actuator_task.adb
@@ -4,14 +4,20 @@ package body Actuator_Task is
 
    task body Actuator is
       Fault : Boolean;
+      Throttle : Float;
    begin
       loop
          Fault := Shared_Obj.Get_Fault;
+         Throttle := Shared_Obj.Get_Throttle;
 
          if Fault then
-            Put_Line("ACTUATOR: >>> BRAKE APPLIED <<<");
+            Put_Line("ACTUATOR: >>> EMERGENCY BRAKE APPLIED <<<");
+         elsif Throttle > 0.0 then
+            Put_Line("ACTUATOR: Accelerating with throttle=" & Float'Image(Throttle));
+         elsif Throttle < 0.0 then 
+            Put_Line("ACTUATOR: Braking with force=" & Float'Image(-Throttle));
          else
-            Put_Line("ACTUATOR: Driving normally");
+            Put_Line("ACTUATOR: Coasting");
          end if;
 
          delay 0.5;
--- src/control_task.adb
+++ src/control_task.adb
@@ -5,18 +5,43 @@ package body Control_Task is
    task body Control is
       Speed    : Float;
       Distance : Float;
+      Target_Speed   : Float;
+      Error_Speed    : Float; 
+      Error_Distance : Float;
+      Throttle       : Float;
+      Safe_Distance  : constant Float := 10.0;
+      Kp_Speed       : constant Float := 0.5;
+      Kp_Distance    : constant Float := 0.2;
+
    begin
       loop
          Speed    := Shared_Obj.Get_Speed;
          Distance := Shared_Obj.Get_Distance;
+         Target_Speed := Shared_Obj.Get_Target_Speed;
 
          if Distance < 10.0 then
             Shared_Obj.Set_Fault(True);
             Put_Line("CONTROL: EMERGENCY!");
+            Throttle := -1.0;
          else
             Shared_Obj.Set_Fault(False);
-            Put_Line("CONTROL: NORMAL");
+            Error_Speed := Target_Speed - Speed;
+            Error_Distance := Distance - Safe_Distance;
+            Throttle := Kp_Speed * Error_Speed + Kp_Distance * Error_Distance;
+            if Throttle > 1.0 then
+               Throttle := 1.0;
+            elsif Throttle < -1.0 then
+               Throttle := -1.0;
+            end if;
+
+            if Throttle > 0.0 then
+               Put_Line("CONTROL: ACCELERATING, throttle=" & Float'Image(Throttle));
+            else
+               Put_Line("CONTROL: Braking, break force =" & Float'Image(-Throttle));
+            end if;
          end if;
+         
+         Shared_Obj.Set_Throttle(Throttle);
 
          delay 0.5;
       end loop;
